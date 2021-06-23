9606c8df794798f34be64a77e3056e0fd03fef25
farah1979 <farahhisham1979@gmail.com>
2021-06-23 14:56:00 +0000

    Add edit/done buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
         done: "select"
       }
     });
+    
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
